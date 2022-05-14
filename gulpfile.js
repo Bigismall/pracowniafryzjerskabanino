2bccf881312f08c374f0d669873cb1e93dbabd2e
Bigismall <rondelious@gmail.com>
2022-05-14 12:07:13 +0200

    reformat gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,10 @@ var gulp = require("gulp");
 var sass = require("gulp-sass");
 
 
-gulp.task('styles', function () {
-    return gulp.src("./stylesheets/*.scss")
-            .pipe(sass({
-                errLogToConsole: true
-            }))
-            .pipe(gulp.dest("./stylesheets"));
-});
+gulp.task('styles', () => gulp.src("./stylesheets/*.scss")
+    .pipe(sass({errLogToConsole: true}))
+    .pipe(gulp.dest("./stylesheets")));
 
-gulp.task("watch", ["styles"], function () {
+gulp.task("watch", ["styles"], () => {
     gulp.watch("./stylesheets/*.scss", ["styles"]);
 });
-
-
